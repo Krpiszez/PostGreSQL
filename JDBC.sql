cc8f59ae285a2584796874a7d1bd61f3ccd4c235
Omer Isik <113728431+Krpiszez@users.noreply.github.com>
2022-12-09 11:23:37 +0100

    Add files via upload

---
+++ JDBC.sql
@@ -0,0 +1,48 @@
+--JDBC PRACTICING CODES ARE IN JAVA
+
+CREATE TABLE countries
+(
+    country_id CHAR(3),
+    country_name VARCHAR(50),
+    region_id SMALLINT
+);
+Insert into countries values ('AR','Argentina',2);
+Insert into countries values ('AU','Australia',3);
+Insert into countries values ('BE','Belgium',1);
+Insert into countries values ('BR','Brazil',2);
+Insert into countries values ('CA','Canada',2);
+Insert into countries values ('CH','Switzerland',1);
+Insert into countries values ('CN','China',3);
+Insert into countries values ('DE','Germany',1);
+Insert into countries values ('DK','Denmark',1);
+Insert into countries values ('EG','Egypt',4);
+Insert into countries values ('FR','France',1);
+Insert into countries values ('IL','Israel',4);
+Insert into countries values ('IN','India',3);
+Insert into countries values ('IT','Italy',1);
+Insert into countries values ('JP','Japan',3);
+Insert into countries values ('KW','Kuwait',4);
+Insert into countries values ('ML','Malaysia',3);
+Insert into countries values ('MX','Mexico',2);
+Insert into countries values ('NG','Nigeria',4);
+Insert into countries values ('NL','Netherlands',1);
+Insert into countries values ('SG','Singapore',3);
+Insert into countries values ('UK','United Kingdom',1);
+Insert into countries values ('US','United States of America',2);
+Insert into countries values ('ZM','Zambia',4);
+Insert into countries values ('ZW','Zimbabwe',4);
+
+select * from countries
+
+--//1.Example: Select the country names whose region id's are 1
+
+select country_name from countries where region_id=1
+
+--//2.Example: Select the country_id and country_name whose region_id's are greater than 2
+select country_id, country_name from countries where region_id>2
+
+--//3.Example: Select the company whose number_of_employees is the lowest from companies table
+
+
+
+
