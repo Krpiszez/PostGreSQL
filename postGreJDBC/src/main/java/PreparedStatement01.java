e9da4efe8c69cda507ffa71d6a6bab93619cca7b
Omer Isik <113728431+Krpiszez@users.noreply.github.com>
2022-12-11 22:20:37 +0100

    Add files via upload

--- postGreJDBC/src/main/java/PreparedStatement01.java
+++ postGreJDBC/src/main/java/PreparedStatement01.java
@@ -1,7 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class PreparedStatement01 {
 
@@ -14,8 +11,43 @@ public class PreparedStatement01 {
         //1.Example: Update the number of employees to 9999 if the company name is IBM by using prepared statement
         //1.Step Create Prepared Statement Query
         String sql1 = "update companies set number_of_employees = ? where company = ?";
-        st.execute(sql1);
 
+        //2.Step Create Prepared Statement Object
+        PreparedStatement pst1 = con.prepareStatement(sql1);
+
+        //3.Step Assign the values by using 'set....()'-.setInt(), .setString() etc.- methods
+        pst1.setInt(1,9999);
+        pst1.setString(2,"IBM");
+
+        //4.Step Execute the Query
+        int numOfRecordsUpdated = pst1.executeUpdate();
+        System.out.println("numOfRecordsUpdated = " + numOfRecordsUpdated);
+
+        String sql2 = "select * from companies";
+        ResultSet rs1 = st.executeQuery(sql2);
+        while (rs1.next()){
+
+            System.out.println(rs1.getInt(1) +"|"+ rs1.getString(2) +"|"+ rs1.getInt(3));
+
+        }
+
+        ////2.Example: Update the number of employees to 5555 if the company name is GOOGLE by using prepared statement
+
+        pst1.setInt(1, 5555);
+        pst1.setString(2, "GOOGLE");
+
+        int numOfRecordsUpdated2 = pst1.executeUpdate();
+        System.out.println("numOfRecordsUpdated2 = " + numOfRecordsUpdated2);
+        ResultSet rs2 = st.executeQuery(sql2);
+        while (rs2.next()){
+
+            System.out.println(rs2.getInt(1) +"|"+ rs2.getString(2) +"|"+ rs2.getInt(3));
+
+        }
+
+        st.close();
+        con.close();
+        pst1.close();
 
     }
 }
